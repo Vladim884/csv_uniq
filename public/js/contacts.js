35928b7271fa48acfecc13fd32c5862869a2f60f
vladimir <vladim.volvenko@gmail.com>
2022-06-27 16:12:21 +0300

    realized active-link in header-menu in main.js

--- public/js/contacts.js
+++ public/js/contacts.js
@@ -1,12 +1,2 @@
-// window.addEventListener('load', function(){
-    const logout_button = document.getElementById('logout')
-    logout_button.classList.add("hidden")
-    const navMenu = document.getElementById('nav-menu')
-        navMenu.childNodes[3].childNodes[0].classList.add("active")
-    
 
-   
-
-    
-// })
 
--- public/js/main.js
+++ public/js/main.js
@@ -2,43 +2,18 @@
 
     const iconBtn = document.getElementById('icon')
     const navMenu = document.getElementById('nav-menu')
-    
-
-
-
-    // let lincsAr = navMenu.getElementsByTagName('li')
-    // console.log(lincsAr)
-
-    function liClickHandler(e) {
-        this.classList.add("active");
-      }
-      //let li = document.querySelector('.li');   получает только первый элемент из массива, так то в данном случае не подходит
-
-// для захвата всех элементов надо использовать  document.querySelectorAll
-
-let li = document.querySelectorAll('.nav-item');
-
-for (let i = 0; i < li.length; i++) { //далее прокручиваем в цикле 
-  li[i].addEventListener("click", function() { // при клике 
-    if (!this.classList.contains('active')) { // проверяем есть ли у элемента, на котором произошло событие, class "active"
-      for (let i = 0; i < li.length; i++) {
-        li[i].classList.remove("active"); // а тут удаляем тупо у всех
-      }
-      this.classList.add("active"); //если нет, добавляем
-      console.log("this - ADD");
-    } else {
-      this.classList.remove("active"); // если есть, удаляем
-      console.log("this - remove");
-    }
-  });
-}
-
-
     iconBtn.onclick = () => {
         navMenu.classList.toggle("vizibly")
     }
 
-    // const navMenu = document.getElementById('nav-menu')
-    // console.log(
-        // navMenu.childNodes[1].childNodes[0].classList.add("active")
-// })
+
+
+    
+// для захвата всех элементов надо использовать  document.querySelectorAll
+let li = document.querySelectorAll('.menu-links .nav-item')
+for (let i = 0; i < li.length; i++) {
+    if(window.location.href === li[i].children[0].href){
+        console.log(li[i].children[0].href)
+        li[i].classList.add('active');
+    }
+}
